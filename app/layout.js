288c5cb6f9f8cdbf318847cba212be4c913745d7
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2025-05-01 11:26:14 +0530

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,7 +55,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+         <Providers>
+        {children}</Providers>
       </body>
     </html>
   );
