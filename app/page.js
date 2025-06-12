c452a8506e612ee76370cbb90c9055ca227fe085
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2025-06-12 17:52:43 +0530

    Update valid wordle link

--- app/page.js
+++ app/page.js
@@ -149,7 +149,7 @@ export default function WordleGame() {
       try {
         // 1. Load word list
         const response = await fetch(
-          'https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt'
+          "https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt"
         );
         const text = await response.text();
         const words = text.split('\n').filter(word => word.length === WORD_LENGTH);
